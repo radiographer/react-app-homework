f8597ac4580fe93ed40703fde9bf438c77568957
radiographer <lukas.w@gmx.com>
2021-06-27 23:38:54 +0200

    ende

--- lesson30/app/src/App.js
+++ lesson30/app/src/App.js
@@ -1,10 +1,34 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Name from "./components/AComponent";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [title, setTitle] = useState("1. tytuł strony.");
+  const [licznik, setLicznik] = useState(2);
+  // let nazwyPorzadkowe = ["Pierwszy", "Drugi", "Trzeci"];
+
+  const onClick = () => {
+    setLicznik(licznik + 1);
+    //  setTitle(nazwyPorzadkowe[licznik] + ". tytuł strony.");
+    setTitle(licznik + ". tytuł strony.");
+  };
+  useEffect(() => {
+    return () => {
+      alert("Teraz był efekt!!");
+    };
+  }, [title]);
+
   return (
     <div className="App">
-      <h1>hello react world</h1>
+      <Name name="Tomek" />
+      <Name name="Romek" />
+      <Name name="Atomek" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <button onClick={onClick}>Zmień tytuł strony</button>
+      {title}
     </div>
   );
 }
